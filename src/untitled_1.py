0190ddb8c9bcd95e1e756ec14e7c337243a9f7d6
Popescu Filimon Andrei Cosmin <andrei.popescufilimon@gmail.com>
2025-02-17 13:31:33 +0200

    Update untitled_1.py

--- src/untitled_1.py
+++ src/untitled_1.py
@@ -12,7 +12,7 @@ sensor.skip_frames(time=2000)
 # **Disable auto settings for stable color tracking**
 sensor.set_auto_gain(False)
 sensor.set_auto_whitebal(False)
-sensor.set_auto_exposure(False, exposure_us=10000)
+sensor.set_auto_exposure(False, exposure_us=8000)  # Adjusted exposure for better visibility
 clock = time.clock()
 
 # **Setup UART Communication**
@@ -23,83 +23,105 @@ red_led = LED(1)
 green_led = LED(2)
 blue_led = LED(3)
 
-# **Indicate Camera Readiness**
-green_led.on()
-red_led.on()  # Blink yellow to indicate qualification mode
-time.sleep(0.5)
-red_led.off()
-green_led.off()
-blue_led.off()
-time.sleep(0.5)
-
-# **Updated Color Thresholds (LAB Space)**
+# **Color Thresholds (LAB Space)**
 red_threshold = [(18, 80, 5, 60, -10, 65)]
 green_threshold = [(20, 85, -60, -5, -5, 55)]
 blue_threshold = [(10, 80, -5, 25, -50, -5)]
-orange_threshold = [(50, 85, 10, 50, 25, 80)]  # **More distinct from red**
+orange_threshold = [(30, 80, 5, 45, 15, 75)]
 
 # **Define Regions of Interest (ROI)**
-cubes_roi = (10, int(sensor.height() / 2), sensor.width() - 20, int(sensor.height() / 2 - 10))
-lines_roi = (5, int(sensor.height() - 60), sensor.width() - 10, 50)
+image_height = sensor.height()
+cubes_roi = (10, int(image_height * 0.6), sensor.width() - 20, int(image_height * 0.4))  # Bottom 40%
+lines_roi = (5, int(image_height * 0.6), sensor.width() - 10, int(image_height * 0.4))  # Bottom 40%
 
 # **Blob Filtering Parameters**
-min_cube_size = 50
-min_line_size = 300
+min_cube_size = 30  # Lowered to capture full objects
+min_line_size = 80  # Lowered to detect thinner lines
+min_area = 10       # Ignore objects with area < 10
 
 def get_largest_blob(blobs):
     """Returns the largest blob in a list or None if empty."""
     return max(blobs, key=lambda b: b.area(), default=None)
+
+def is_elongated(blob):
+    """Check if the blob is at least 5 times taller than its width."""
+    return blob.w() >= 5 * blob.h()
+
+def is_invalid_orange(orange_blob, red_blobs):
+    if orange_blob.h() > orange_blob.w():
+        return "ignore_orange"  # Ignore if taller than wide
+
+    for red_blob in red_blobs:
+        if (
+            red_blob.x() < orange_blob.x() + orange_blob.w() and
+            red_blob.x() + red_blob.w() > orange_blob.x() and
+            red_blob.y() < orange_blob.y() + orange_blob.h() and
+            red_blob.y() + red_blob.h() > orange_blob.y()
+        ):
+            red_area = red_blob.area()
+            orange_area = orange_blob.area()
+
+            if red_area >= orange_area * 1.5:  # 50% larger or more
+                return "ignore_orange"
+            elif orange_area >= red_area * 1.5:  # 50% larger or more
+                return "ignore_red"
+            else:
+                return None
+
+    return None  # No overlap, keep both
+
 
 direction = 0
 
 while True:
     img = sensor.snapshot()
 
-    # **Detect Colored Cubes**
+    # **Detect Red & Green Cubes**
     red_blobs = img.find_blobs(red_threshold, roi=cubes_roi, pixels_threshold=min_cube_size, area_threshold=min_cube_size, merge=True)
     green_blobs = img.find_blobs(green_threshold, roi=cubes_roi, pixels_threshold=min_cube_size, area_threshold=min_cube_size, merge=True)
 
-    # **Find Largest Red & Green Cube**
-    red_cube = get_largest_blob(red_blobs)
-    green_cube = get_largest_blob(green_blobs)
-
-    # **Detect Colored Lines**
+    # **Detect Blue & Orange Lines**
     blue_blobs = img.find_blobs(blue_threshold, roi=lines_roi, pixels_threshold=min_line_size, area_threshold=min_line_size, merge=True)
     orange_blobs = img.find_blobs(orange_threshold, roi=lines_roi, pixels_threshold=min_line_size, area_threshold=min_line_size, merge=True)
 
-    # **Find Widest Blue & Orange Line**
-    blue_line = get_largest_blob(blue_blobs)
-    orange_line = get_largest_blob(orange_blobs)
+    # **Find Largest Objects**
+    red_cube = get_largest_blob([b for b in red_blobs if b.area() >= min_area])
+    green_cube = get_largest_blob([b for b in green_blobs if b.area() >= min_area])
+    blue_line = get_largest_blob([b for b in blue_blobs if b.area() >= min_area])
+    orange_line = get_largest_blob([b for b in orange_blobs if b.area() >= min_area])
 
-    # **Process Red Cube**
-    if red_cube:
+    # **Process Red Cube (Ignore if elongated or too small)**
+    if red_cube and not is_elongated(red_cube):
         img.draw_rectangle(red_cube.rect(), color=(255, 0, 0))
         img.draw_cross(red_cube.cx(), red_cube.cy(), color=(255, 0, 0))
-        uart.write("RED Cube Detected at X:{} Y:{}\n".format(red_cube.cx(), red_cube.cy()))
+        img.draw_string(red_cube.x(), red_cube.y() + red_cube.h() - 10, str(red_cube.area()), color=(255, 0, 0))
+        uart.write("RED Cube Detected at X:{} Y:{} Area:{}\n".format(red_cube.cx(), red_cube.cy(), red_cube.area()))
 
-    # **Process Green Cube**
-    if green_cube:
+    # **Process Green Cube (Ignore if elongated or too small)**
+    if green_cube and not is_elongated(green_cube):
         img.draw_rectangle(green_cube.rect(), color=(0, 255, 0))
         img.draw_cross(green_cube.cx(), green_cube.cy(), color=(0, 255, 0))
-        uart.write("GREEN Cube Detected at X:{} Y:{}\n".format(green_cube.cx(), green_cube.cy()))
+        img.draw_string(green_cube.x(), green_cube.y() + green_cube.h() - 10, str(green_cube.area()), color=(0, 255, 0))
+        uart.write("GREEN Cube Detected at X:{} Y:{} Area:{}\n".format(green_cube.cx(), green_cube.cy(), green_cube.area()))
 
-    # **Process Blue Line**
+    # **Process Blue Line (Ignore if too small)**
     if blue_line:
         img.draw_rectangle(blue_line.rect(), color=(0, 0, 255))
-        uart.write("BLUE Line Detected at X:{} Width:{}\n".format(blue_line.cx(), blue_line.w()))
+        img.draw_string(blue_line.x(), blue_line.y() + blue_line.h() - 10, str(blue_line.area()), color=(0, 0, 255))
+        uart.write("BLUE Line Detected at X:{} Width:{} Area:{}\n".format(blue_line.cx(), blue_line.w(), blue_line.area()))
 
-    # **Process Orange Line**
-    if orange_line:
-        img.draw_rectangle(orange_line.rect(), color=(255, 165, 0))
-        uart.write("ORANGE Line Detected at X:{} Width:{}\n".format(orange_line.cx(), orange_line.w()))
+    # **Process Orange Line (Ignore if invalid or too small)**
+    if orange_line and not is_invalid_orange(orange_line, red_blobs):
+        img.draw_rectangle(orange_line.rect(), color=(255, 165, 0))  # Orange color
+        img.draw_string(orange_line.x(), orange_line.y() + orange_line.h() - 10, str(orange_line.area()), color=(255, 165, 0))
+        uart.write("ORANGE Line Detected at X:{} Width:{} Area:{}\n".format(orange_line.cx(), orange_line.w(), orange_line.area()))
 
     # **Determine Direction Based on First Detected Line**
     if direction == 0:
-        if orange_line:
+        if orange_line and not is_invalid_orange(orange_line, red_blobs):
             direction = 2  # Orange line first → turn right
         elif blue_line:
             direction = 1  # Blue line first → turn left
 
     # **Send Direction Command to Arduino**
-    has_line = orange_line or blue_line
     uart.write(str(direction) + '\n')
